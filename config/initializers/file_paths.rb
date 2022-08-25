824254338294ee58ed62221113b53f7d3b180765
lcjohnso <lcj@northwestern.edu>
2022-08-25 16:20:10 -0500

    update img urls to https

--- config/initializers/file_paths.rb
+++ config/initializers/file_paths.rb
@@ -1,13 +1,13 @@
 #
 # File path constants
 #
-WEB_URL_LORI    = 'http://uw-phat-lcj.s3-website-us-east-1.amazonaws.com/Dec2014_runs/'
-WEB_URL         = 'http://uw-phat-lcj.s3-website-us-east-1.amazonaws.com'
+WEB_URL_LORI    = 'https://uw-phat-lcj.s3.amazonaws.com/Dec2014_runs/'
+WEB_URL         = 'https://uw-phat-lcj.s3.amazonaws.com'
 #WEB_URL_LORI    = 'http://www.astro.washington.edu/users/beermalc/Dec2014_runs/'
 #WEB_URL         = 'http://www.astro.washington.edu/users/lcjohnso/phat'
 IMAGE_DIR       = '/cutouts'
 IMAGE_DIR_AP    = '/ap_cutouts'
 CMD_DIR_AP      = '/ap_cmds'
-NC_URL_PRE      = 'http://www.cfa.harvard.edu/oir/eg/m31clusters/phat/'
+NC_URL_PRE      = 'https://lweb.cfa.harvard.edu/oir/eg/m31clusters/phat/'
 NC_URL_POST     = '_mctp.html'
 IMG_DIR_PROFILE = '/ap_profiles'
